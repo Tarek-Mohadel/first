9b97d3618a614d579306af240512e68e6dfba8c4
Tarek-Mohadel <93818616+Tarek-Mohadel@users.noreply.github.com>
2024-06-29 19:56:34 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,10 @@ import streamlit as st
 # Show the page title and description.
 st.set_page_config(page_title="Movies dataset", page_icon="🎬")
 st.title("🎬 Movies dataset")
+
+data = pd.DataFrame({
+    'latitude': [37.7749, 34.0522, 40.7128],
+    'longitude': [-122.4194, -118.2437, -74.0060]
+})
+
+st.map(data)
